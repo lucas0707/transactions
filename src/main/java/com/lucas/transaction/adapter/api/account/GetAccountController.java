fb01eaca601d13be14662a9e318ca62b4de5f1cb
Lucas Machado <lucasfl.machado@gmail.com>
2021-08-07 16:17:41 -0300

    Added swagger documentation

--- src/main/java/com/lucas/transaction/adapter/api/account/GetAccountController.java
+++ src/main/java/com/lucas/transaction/adapter/api/account/GetAccountController.java
@@ -1,13 +1,13 @@
 package com.lucas.transaction.adapter.api.account;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetAccountController {
 
-    @RequestMapping("/")
+    @GetMapping("/account")
     public @ResponseBody String getAccountDetails()
     {
         return "hello world";
